0fd3e0fadc2cf1e3f38c680ba0e86bdf91b50da4
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-11-14 17:51:51 -0500

    Test cases added to Square root exercise

--- 1. square-root-interger.py
+++ 1. square-root-interger.py
@@ -2,6 +2,9 @@
 #     return int(number ** (1 / 2))
 
 def sqrt(number):  # O(logn)
+    if number < 0:
+        print(number, ' is invalid, must be greater than or equal to 0')
+        return -1
     result = 0
     while (result + 1) ** 2 <= number:
         result += 1
@@ -9,8 +12,12 @@ def sqrt(number):  # O(logn)
 
 
 if __name__ == '__main__':
+    print("Pass" if (-1 == sqrt(-120)) else "Fail")
     print("Pass" if (3 == sqrt(9)) else "Fail")
     print("Pass" if (0 == sqrt(0)) else "Fail")
     print("Pass" if (4 == sqrt(16)) else "Fail")
     print("Pass" if (1 == sqrt(1)) else "Fail")
     print("Pass" if (5 == sqrt(27)) else "Fail")
+    print("Pass" if (31 == sqrt(999)) else "Fail")
+    print("Pass" if (92 == sqrt(8572)) else "Fail")
+    print("Pass" if (436 == sqrt(190292)) else "Fail")
