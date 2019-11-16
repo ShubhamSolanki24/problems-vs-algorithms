7748d355e04743db6787c4495e939a0e62c7405a
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-11-16 15:40:15 -0500

    Max min unsorted array edge cases added

--- 6. max-min-unsorted-array.py
+++ 6. max-min-unsorted-array.py
@@ -8,14 +8,15 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-    if len(ints) == 0:
-        print('Invalid Length of ints')
+
+    if ints is None or len(ints) == 0:
+        print('Invalid input:', ints)
         return float('inf'), float('-inf')
 
     min_found = ints[0]
     max_found = ints[0]
 
-    for item in ints[1:]:
+    for item in ints:
         if item < min_found:
             min_found = item
 
@@ -26,14 +27,21 @@ def get_min_max(ints):
 
 
 if __name__ == '__main__':
-    test1 = [i for i in range(0, 10)]  # a list containing 0 - 9
+    test1 = [i for i in range(0, 10)]
     random.shuffle(test1)
     print("Pass" if ((0, 9) == get_min_max(test1)) else "Fail")
 
-    test2 = [i for i in range(0, 100)]  # a list containing 0 - 9
+    test2 = [i for i in range(0, 100)]
     random.shuffle(test2)
     print("Pass" if ((0, 99) == get_min_max(test2)) else "Fail")
 
-    test3 = [i for i in range(0, 1000)]  # a list containing 0 - 9
+    test3 = [i for i in range(0, 1000)]
     random.shuffle(test3)
     print("Pass" if ((0, 999) == get_min_max(test3)) else "Fail")
+
+    # Edge cases
+    print("Pass" if ((-9, 0) == get_min_max([i for i in range(0, -10, -1)])) else "Fail")
+    print("Pass" if ((0, 0) == get_min_max([0])) else "Fail")
+    print("Pass" if ((float('inf'), float('-inf')) == get_min_max([])) else "Fail")
+    print("Pass" if ((float('inf'), float('-inf')) == get_min_max(None)) else "Fail")
+    print("Pass" if ((-100, 99) == get_min_max([i for i in range(-100, 100)])) else "Fail")
